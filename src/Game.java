2a6dae3b457d0533a57ed6dc722911438698c10c
Morrighan <xmorrighan@gmail.com>
2023-04-22 17:13:33 +1000

    Goblins created, they will now spawn at random places near the character at a random interval between 3 and 10 seconds

--- src/Game.java
+++ src/Game.java
@@ -1,148 +1,193 @@
-import Modules.Background;
-import Modules.Character;
-import Modules.Music;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.awt.geom.AffineTransform;
+    import Modules.Background;
+    import Modules.Character;
+    import Modules.Music;
+    import javax.swing.*;
+    import java.awt.*;
+    import java.awt.event.*;
+    import java.awt.geom.AffineTransform;
+    import Modules.Goblin;
+    import java.util.ArrayList;
+    import java.util.Random;
 
-
-public class Game extends JPanel implements KeyListener, ActionListener, MouseWheelListener {
-    private double cameraX, cameraY;
-    private double zoomFactor = 1.0;
-    private final Character character;
-    private final Background background;
-    private final Music music;
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-        // You can leave this method empty if you don't want to use it
-    }
-
-    public Game() {
-        setPreferredSize(new Dimension(800, 600));
-        addKeyListener(this);
-        setFocusable(true);
-        setDoubleBuffered(true);
-        addMouseWheelListener(this);
-
-        background = new Background("src/resources/grassy_field_tile.png");
-        int centerX = (background.getImage().getWidth() * 5) / 2;
-        int centerY = (background.getImage().getHeight() * 5) / 2;
-
-        character = new Character(centerX - 10, centerY - 10, 5, "src/resources/KnightScaled.png"); // Subtract half of character size (20/2) to center it
-
-        cameraX = centerX - (double) getWidth() / 2;
-        cameraY = centerY - (double) getHeight() / 2;
-
-        music = new Music("src/resources/Nightfall Invasion.wav");
-        // Comment or remove the following line to prevent the music from looping
-        // music.loop();
-
-        Timer timer = new Timer(1000 / 60, this);
-        timer.start();
-    }
-
-    private void updateCamera() {
-        cameraX = character.getX() - getWidth() / (2 * zoomFactor);
-        cameraY = character.getY() - getHeight() / (2 * zoomFactor);
-    }
-
-
-    public void moveCharacter() {
-        character.move();
-        repaint();
-    }
-
-    public void keyPressed(KeyEvent e) {
-        int keyCode = e.getKeyCode();
-        if (keyCode == KeyEvent.VK_W) {
-            character.setDy(-character.getSpeed());
-        } else if (keyCode == KeyEvent.VK_S) {
-            character.setDy(character.getSpeed());
-        } else if (keyCode == KeyEvent.VK_A) {
-            character.setDx(-character.getSpeed());
-        } else if (keyCode == KeyEvent.VK_D) {
-            character.setDx(character.getSpeed());
+    public class Game extends JPanel implements KeyListener, ActionListener, MouseWheelListener {
+        private double cameraX, cameraY;
+        private double zoomFactor = 1.0;
+        private final Character character;
+        private final Background background;
+        private final Music music;
+        private final ArrayList<Goblin> goblins;
+        @Override
+        public void keyTyped(KeyEvent e) {
+            // You can leave this method empty if you don't want to use it
         }
-    }
 
-    public void keyReleased(KeyEvent e) {
-        int keyCode = e.getKeyCode();
-        if (keyCode == KeyEvent.VK_W && character.getDy() == -character.getSpeed()) {
-            character.setDy(0);
-        } else if (keyCode == KeyEvent.VK_S && character.getDy() == character.getSpeed()) {
-            character.setDy(0);
-        } else if (keyCode == KeyEvent.VK_A && character.getDx() == -character.getSpeed()) {
-            character.setDx(0);
-        } else if (keyCode == KeyEvent.VK_D && character.getDx() == character.getSpeed()) {
-            character.setDx(0);
+        private Timer goblinSpawner;
+
+
+        public Game() {
+            setPreferredSize(new Dimension(800, 600));
+            addKeyListener(this);
+            setFocusable(true);
+            setDoubleBuffered(true);
+            addMouseWheelListener(this);
+
+            background = new Background("src/resources/grassy_field_tile.png");
+            int centerX = (background.getImage().getWidth() * 5) / 2;
+            int centerY = (background.getImage().getHeight() * 5) / 2;
+
+            character = new Character(centerX - 10, centerY - 10, 5, "src/resources/KnightScaled.png");
+
+            cameraX = centerX - (double) getWidth() / 2;
+            cameraY = centerY - (double) getHeight() / 2;
+
+            music = new Music("src/resources/Nightfall Invasion.wav");
+
+            goblins = new ArrayList<>();
+
+            Timer timer = new Timer(1000 / 60, this);
+            timer.start();
+
+            goblinSpawner = new Timer(3000, null);
+            goblinSpawner.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    spawnGoblin();
+                    int delay = 3000 + new Random().nextInt(7000);
+                    goblinSpawner.setInitialDelay(delay);
+                    goblinSpawner.restart();
+                }
+            });
+            goblinSpawner.setRepeats(true); // Change this line
+            goblinSpawner.start();
         }
-    }
 
 
-    public void actionPerformed(ActionEvent e) {
-        moveCharacter();
-        updateCamera();
-    }
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Kingdom of the Night");
-        Game game = new Game();
-        frame.add(game);
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-    }
-
-
-    public void mouseWheelMoved(MouseWheelEvent e) {
-        int notches = e.getWheelRotation();
-        double zoomAmount = 0.1;
-
-        if (notches < 0) {
-            zoomFactor += zoomAmount;
-        } else {
-            zoomFactor -= zoomAmount;
-            if (zoomFactor < zoomAmount) {
-                zoomFactor = zoomAmount;
-            }
+        private void updateCamera() {
+            cameraX = character.getX() - getWidth() / (2 * zoomFactor);
+            cameraY = character.getY() - getHeight() / (2 * zoomFactor);
         }
-    }
 
 
+        public void moveCharacter() {
+            character.move();
+            repaint();
+        }
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
-
-        AffineTransform oldTransform = g2d.getTransform();
-
-        g2d.scale(zoomFactor, zoomFactor);
-        g2d.translate(-cameraX, -cameraY);
-
-        // Draw the background
-        int imgWidth = background.getImage().getWidth();
-        int imgHeight = background.getImage().getHeight();
-        int startX = (int) Math.floor(cameraX / imgWidth) * imgWidth;
-        int startY = (int) Math.floor(cameraY / imgHeight) * imgHeight;
-        int numTilesX = (int) Math.ceil(getWidth() / (double) imgWidth / zoomFactor) + 1;
-        int numTilesY = (int) Math.ceil(getHeight() / (double) imgHeight / zoomFactor) + 1;
-        for (int x = 0; x < numTilesX; x++) {
-            for (int y = 0; y < numTilesY; y++) {
-                g2d.drawImage(background.getImage(), startX + x * imgWidth, startY + y * imgHeight, null);
+        public void keyPressed(KeyEvent e) {
+            int keyCode = e.getKeyCode();
+            if (keyCode == KeyEvent.VK_W) {
+                character.setDy(-character.getSpeed());
+            } else if (keyCode == KeyEvent.VK_S) {
+                character.setDy(character.getSpeed());
+            } else if (keyCode == KeyEvent.VK_A) {
+                character.setDx(-character.getSpeed());
+            } else if (keyCode == KeyEvent.VK_D) {
+                character.setDx(character.getSpeed());
             }
         }
 
-        // Draw the character
-        int charWidth = character.getImage().getWidth();
-        int charHeight = character.getImage().getHeight();
-        g2d.drawImage(character.getImage(), character.getX() - charWidth / 2, character.getY() - charHeight / 2, null);
+        public void keyReleased(KeyEvent e) {
+            int keyCode = e.getKeyCode();
+            if (keyCode == KeyEvent.VK_W && character.getDy() == -character.getSpeed()) {
+                character.setDy(0);
+            } else if (keyCode == KeyEvent.VK_S && character.getDy() == character.getSpeed()) {
+                character.setDy(0);
+            } else if (keyCode == KeyEvent.VK_A && character.getDx() == -character.getSpeed()) {
+                character.setDx(0);
+            } else if (keyCode == KeyEvent.VK_D && character.getDx() == character.getSpeed()) {
+                character.setDx(0);
+            }
+        }
 
-        g2d.setTransform(oldTransform);
+
+        public void actionPerformed(ActionEvent e) {
+            moveCharacter();
+            updateCamera();
+        }
+
+        public static void main(String[] args) {
+            JFrame frame = new JFrame("Kingdom of the Night");
+            Game game = new Game();
+            frame.add(game);
+            frame.pack();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        }
+
+
+        public void mouseWheelMoved(MouseWheelEvent e) {
+            int notches = e.getWheelRotation();
+            double zoomAmount = 0.1;
+
+            if (notches < 0) {
+                zoomFactor += zoomAmount;
+            } else {
+                zoomFactor -= zoomAmount;
+                if (zoomFactor < zoomAmount) {
+                    zoomFactor = zoomAmount;
+                }
+            }
+        }
+
+
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2d = (Graphics2D) g;
+
+            AffineTransform oldTransform = g2d.getTransform();
+
+            g2d.scale(zoomFactor, zoomFactor);
+            g2d.translate(-cameraX, -cameraY);
+
+            // Draw the background
+            int imgWidth = background.getImage().getWidth();
+            int imgHeight = background.getImage().getHeight();
+            int startX = (int) Math.floor(cameraX / imgWidth) * imgWidth;
+            int startY = (int) Math.floor(cameraY / imgHeight) * imgHeight;
+            int numTilesX = (int) Math.ceil(getWidth() / (double) imgWidth / zoomFactor) + 1;
+            int numTilesY = (int) Math.ceil(getHeight() / (double) imgHeight / zoomFactor) + 1;
+            for (int x = 0; x < numTilesX; x++) {
+                for (int y = 0; y < numTilesY; y++) {
+                    g2d.drawImage(background.getImage(), startX + x * imgWidth, startY + y * imgHeight, null);
+                }
+            }
+
+            int charWidth = character.getImage().getWidth();
+            int charHeight = character.getImage().getHeight();
+            g2d.drawImage(character.getImage(), character.getX() - charWidth / 2, character.getY() - charHeight / 2, null);
+
+            // Draw the goblins
+            for (Goblin goblin : goblins) {
+                int goblinWidth = goblin.getImage().getWidth();
+                int goblinHeight = goblin.getImage().getHeight();
+                g2d.drawImage(goblin.getImage(), goblin.getX() - goblinWidth / 2, goblin.getY() - goblinHeight / 2, null);
+            }
+
+            g2d.setTransform(oldTransform);
+        }
+
+
+        private void spawnGoblin() {
+            Random random = new Random();
+            int minDistance = 200;
+            int maxDistance = 500;
+
+            int distanceX = minDistance + random.nextInt(maxDistance - minDistance);
+            int distanceY = minDistance + random.nextInt(maxDistance - minDistance);
+
+            distanceX = random.nextBoolean() ? distanceX : -distanceX;
+            distanceY = random.nextBoolean() ? distanceY : -distanceY;
+
+            int x = character.getX() + distanceX;
+            int y = character.getY() + distanceY;
+
+            Goblin goblin = new Goblin(x, y, 3, "src/resources/GoblinScaled.png");
+            goblins.add(goblin);
+        }
+
+
     }
-
-
-}
---
+++ src/Modules/Goblin.java
@@ -0,0 +1,10 @@
+// src/Modules/Goblin.java
+package Modules;
+
+public class Goblin extends Character {
+
+    public Goblin(int x, int y, int speed, String imagePath) {
+        super(x, y, speed, imagePath);
+    }
+
+}
